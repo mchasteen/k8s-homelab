0c0e8e4b70fd0b42e8dc6f589436d194f6e97845
Micah Chasteen <3074234+mchasteen@users.noreply.github.com>
2023-09-05 17:24:54 -0600

    Added updated configuration for longhorn that fixes taking snapshots.  Kasten was unable to take a snapshot of a longhorn pvc due to the volumestorageclass configuration.

--- envrionment/longhorn/install-longhorn.sh
+++ envrionment/longhorn/install-longhorn.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-helm repo add longhorn https://charts.longhorn.io
-helm repo update
-helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.4.1
+#helm repo add longhorn https://charts.longhorn.io
+#helm repo update
+#helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.4.1
 #helm install longhorn longhorn/longhorn \
 #  --namespace longhorn-system \
 #  --create-namespace \
 #  --values values.yaml
+
+
+helm upgrade --install longhorn longhorn \
+  --repo  https://charts.longhorn.io \
+  --namespace longhorn-system --create-namespace \
+  --version 1.5.1
--- envrionment/longhorn/install-snapshot-controller.sh
+++ envrionment/longhorn/install-snapshot-controller.sh
@@ -3,20 +3,20 @@
 
 mkdir -p client/config/crd/
 
-wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/kustomization.yaml -O client/config/crd/kustomization.yaml
-wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml -O client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
-wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml -O client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
-wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml -O client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
+wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/v6.2.2/client/config/crd/kustomization.yaml -O client/config/crd/kustomization.yaml
+wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/v6.2.2/client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml -O client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
+wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/v6.2.2/client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml -O client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
+wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/v6.2.2/client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml -O client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
 
-kubectl kustomize client/config/crd | kubectl create -f -
+kubectl kustomize client/config/crd | kubectl apply -f -
 
 
 mkdir -p deploy/kubernetes/snapshot-controller/
 
-wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/deploy/kubernetes/snapshot-controller/kustomization.yaml -O deploy/kubernetes/snapshot-controller/kustomization.yaml
-wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml -O deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml
-wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml -O deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
+wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/v6.2.2/deploy/kubernetes/snapshot-controller/kustomization.yaml -O deploy/kubernetes/snapshot-controller/kustomization.yaml
+wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/v6.2.2/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml -O deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml
+wget https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/v6.2.2/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml -O deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
 
-kubectl -n kube-system kustomize deploy/kubernetes/snapshot-controller | kubectl create -f -
+kubectl -n kube-system kustomize deploy/kubernetes/snapshot-controller | kubectl apply -f -
 
 kubectl apply -f volume-snapshot-class.yaml
